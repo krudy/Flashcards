36be8dcb2f9e6a00c4d4243bbe87a7e153426f71
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-02-29 21:13:18 +0100

    action -getAllFlashacards added

--- actions/api/flashcardActions.js
+++ actions/api/flashcardActions.js
@@ -1,19 +1,26 @@
-const Word = require('../../db/models/flashcard');
+const Flashcard = require('../../db/models/flashcard');
 
 class FlashcardActions {
     saveFlashcard(req, res) {
         const polishWord = req.body.polishWord;
-           const englishWord = req.body.englishWord;
-           const type = req.body.type;  
-           const polishExample = req.body.polishExample;
-           const englishExample = req.body.englishExample;    
-        
+        const englishWord = req.body.englishWord;
+        const type = req.body.type;
+        const polishExample = req.body.polishExample;
+        const englishExample = req.body.englishExample;
+
 
         res.send(`word has been added successfully PL ${polishWord} EN ${englishWord} type ${type}`);
     }
 
-    getAllFlashcards(req, res) {
-        res.send('api is working');
+    async getAllFlashcards(req, res) {
+        let docs;
+        try{
+        docs = await Flashcard.find({});
+        }catch(e){
+            return res.status(500).json({message: e.message});
+        }
+        
+        res.status(200).json(docs);
     }
 
     getRandomFlashcard(req, res) {
