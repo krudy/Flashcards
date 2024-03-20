f5fed119ca54b3ab3f9cb41374734110e125bfc4
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-03-20 09:10:06 +0100

    added a 'player eligibility check' to the list of winners

--- frontend/src/components/Flashcards/Flashcards.js
+++ frontend/src/components/Flashcards/Flashcards.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Flashcards.css';
 import Flashcard from './Flashcard/Flashcard';
-import NewFlashcard  from './NewFlashcard/NewFlashcard';
+import NewFlashcard from './NewFlashcard/NewFlashcard';
+import NewResult from '../Results/NewResult/NewResult';
 import axios from 'axios';
 
 class Flashcards extends React.Component {
@@ -10,89 +11,118 @@ class Flashcards extends React.Component {
         super(props);
 
         //referance to next button
-        this.nextButtonRef = React.createRef(); 
+        this.nextButtonRef = React.createRef();
 
         this.state = {
-            
+
             flashcards: [],
             points: 0,
             game: 0,
-            gameOver: false
+            gameOver: false,
+            isTheWinner: false,
+           
+
         }
 
-        
+
     }
 
-    
+
     componentDidMount() {
-        this.fetchNotes();     
-    }
-    
-    async fetchNotes() {  
-    const cards = [];
-    for(let i = 0; i < 2; i++) {
-        const res =  await axios.get('http://localhost:9999/api/flashcards/random');
-        const card = res.data;
-        card.isCorrect = false; 
-        cards.push(card);
-    }
-      this.setState({flashcards: cards});
+        this.fetchNotes();
     }
 
-    
+    async fetchNotes() {
+        const cards = [];
+        for (let i = 0; i < 2; i++) {
+            const res = await axios.get('http://localhost:9999/api/flashcards/random');
+            const card = res.data;
+            card.isCorrect = false;
+            cards.push(card);
+        }
+        this.setState({ flashcards: cards });
+    }
+
+
 
     CheckFlashcard(isCorrect) {
         if (isCorrect) {
             this.setState(prevState => ({ points: prevState.points + 1 }));
-            this.setState(prevState => ({ game: prevState.game + 1}));
-            
+            this.setState(prevState => ({ game: prevState.game + 1 }));
+
         }
-        
+
     }
 
     async AddFlashcard(flashcard) {
-       await axios.post('http://localhost:9999/api/flashcards', flashcard);
+        await axios.post('http://localhost:9999/api/flashcards', flashcard);
     }
 
-    handleNextButton() {
-        
-        if (this.state.game === 0 ) {
-            this.nextButtonRef.current.disabled = true;
-            this.setState({ gameOver: true });
-            return;
+    async checkIsTheWinner(points) {
+        const res = await axios.get('http://localhost:9999/api/results');
+        const results = res.data;
+        console.log(results);
+  
+        const isWinner = results.some(result => points > result.score);
+        if(isWinner){
+            this.setState({isTheWinner: true})
         }
-        // clear flashcards state
-        this.setState({ flashcards: [] }); 
-        // get new flashcards
-        this.fetchNotes(); 
-        if(this.state.game === 2){
-            this.setState(prevState => ({ points: prevState.points + 1 }));
-        }
-        this.setState({ game: 0});
-        
+            
+            console.log('wartosc winner' + isWinner);
+            
+
+       
+        return results;
     }
 
-    render() {
+    async saveResult(result) {
+        const res = await axios.post('http://localhost:9999/api/results', result);
+        const results = await this.checkIsTheWinner(res.data);
+        this.setState({ results: results });
+    }
+
+
+
+    async handleNextButton() {
+
+    if (this.state.game === 0) {
+        this.nextButtonRef.current.disabled = true;
+        this.setState({ gameOver: true });
+        this.checkIsTheWinner(this.state.points)
+        return;
+    }
+    // clear flashcards state
+    this.setState({ flashcards: [] });
+    // get new flashcards
+    this.fetchNotes();
+    if (this.state.game === 2) {
+        this.setState(prevState => ({ points: prevState.points + 1 }));
+    }
+    this.setState({ game: 0 });
+
+}
+
+render() {
 
 
 
 
-        return (
-            <div>
+    return (
+        <div>
 
-                <h1>Flashcards</h1>
+            <h1>Flashcards</h1>
 
 
-                <NewFlashcard
-                    onAdd={(flashcard) => this.AddFlashcard(flashcard)}/>
+            <NewFlashcard
+                onAdd={(flashcard) => this.AddFlashcard(flashcard)} />
 
-                    <p className='points'>Points: {this.state.points}</p>
-                    {this.state.gameOver && <div className="gameOverMessage" ><h2>GAME OVER</h2></div>}
-                <div className='flashcardsContainer'>
-                    {this.state.flashcards.map(flashcard => {
-                       
-                        return (
-                           <Flashcard 
+            <p className='points'>Points: {this.state.points}</p>
+            {this.state.gameOver && <div className="gameOverMessage" ><h2>GAME OVER</h2></div>}
+            <div className='flashcardsContainer'>
+                {this.state.flashcards.map(flashcard => {
+
+                    return (
+                        <Flashcard
                             key={flashcard._id}
                             id={flashcard._id}
                             polishWord={flashcard.polishWord}
@@ -100,18 +130,21 @@ class Flashcards extends React.Component {
                             polishExample={flashcard.polishExample}
                             englishExample={flashcard.englishExample}
                             type={flashcard.type}
-                            onCheck={(isCorrect) => this.CheckFlashcard(flashcard._id, isCorrect)} 
+                            onCheck={(isCorrect) => this.CheckFlashcard(flashcard._id, isCorrect)}
                             bonus={flashcard.bonus}
-                            />
+                        />
 
-                        )
-                    })}
+                    )
+                })}
 
-                </div>
-                <button ref={this.nextButtonRef} onClick={() => this.handleNextButton()} className='Next' >NEXT</button>
             </div>
-        );
-    }
+            <button ref={this.nextButtonRef} onClick={() => this.handleNextButton()} className='Next' >NEXT</button>
+
+            {/* Wyświetlamy komponent NewResult po zakończeniu gry */}
+            {this.state.isTheWinner && <NewResult />}
+        </div>
+    );
+}
 }
 
 export default Flashcards;
